c39efd57b6dca4e5024f71d5f2f9c8f0d2b1290e
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2025-01-21 14:47:00 +0100

    feat(app): création Layout par défaut et homepage index.vue

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+    <div>
+        <header>
+            <!-- <Header /> -->
+        </header>
+
+        <main>
+            <slot />
+        </main>
+
+        <footer>
+            <!-- <Footer /> -->
+        </footer>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+
+</script>
+
+
+<template>
+<div class="mx-page">
+    <h1>Cyber Showdown</h1>
+</div>
+</template>
+
+
+<style lang='scss'>
+
+</style>
